a1be15c9aeb1260e84a799fcf5f73f04f39d4202
duyan38 <duyanpx2001@gmail.com>
2024-02-29 20:05:32 +0700

    khoi tao fw va lam them fe

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def base(requert):
+    return render(requert,'user/base.html')
+
+def home(requert):
+    return render(requert,'user/home.html')
+
+def category(requert):
+    return render(requert,'user/category.html')
+
+def detai(requert):
+    return render(requert,'user/detai.html')
